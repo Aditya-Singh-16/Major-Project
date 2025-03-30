69bf24158b69a4e9605949bae5931bd2f0298a0c
aditya <ashraysingh812gmail.com>
2025-03-30 21:56:19 +0530

    master branch

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Navbar/>
+    </>
   )
 }
