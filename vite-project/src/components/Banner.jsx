ada6a30795730bc2d9d157d6f6d211265bd7c496
aditya <ashraysingh812gmail.com>
2025-04-07 19:40:19 +0530

    master branch

--- vite-project/src/components/Banner.jsx
+++ vite-project/src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Mail } from 'lucide-react';
 
 // Use your image path
 const bannerImage = new URL('../assets/tori.jpg', import.meta.url).href;
@@ -14,6 +15,7 @@ function Banner() {
         viewport={{ once: true }}
         className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center gap-10"
       >
+      <div>  
         {/* Left Side */}
         <div className="w-full md:w-1/2 space-y-6 md:space-y-10 mt-6 md:mt-12">
           <h1 className="text-3xl md:text-5xl font-extrabold text-slate-800 leading-tight">
@@ -27,13 +29,19 @@ function Banner() {
             student seeking academic resources, we have something for everyone. Start your reading journey today! 📚✨
           </p>
         </div>
-
+`       <label className="input input-bordered flex items-center gap-2">
+        <Mail className="w-5 h-5 test-gray-500"/>
+        <input type="text" className="grow" placeholder="Enter your email to login" />
+      ` </label>
+        <button className="btn m-3 btn-secondary">Secondary</button>
+        </div>
+        
         {/* Right Side */}
-        <div className="w-full md:w-1/2 flex justify-center items-center">
+        <div className="w-full md:w-10/12 flex justify-center items-center">
           <motion.img
             src={bannerImage}
             alt="Book Banner"
-            className="w-full max-w-sm rounded-2xl shadow-xl object-cover border border-gray-200"
+            className="w-80 h-92 max-w-sm rounded-2xl shadow-xl object-cover border border-gray-200"
             initial={{ opacity: 0, scale: 0.9 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.8, ease: 'easeOut', delay: 0.3 }}
